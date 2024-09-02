e87ad300113c3f2174565bccf9204ade79474f22
Galen Gong <galengong@gmail.com>
2024-09-02 19:11:10 -0400

    Refactor code into main and normalization functions

--- spark/normalization.py
+++ spark/normalization.py
@@ -2,39 +2,42 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, min as spark_min, max as spark_max
 
 
-# Initialize Spark session
-spark = (
-    SparkSession.builder.appName("ExampleApp")  # type: ignore
-    .master("spark://localhost:7077")
-    .getOrCreate()
-)
+def normalize_dataframe(df):
+    # Compute the minimum and maximum values of the "Value" column
+    min_value = df.select(spark_min(col("Value"))).collect()[0][0]
+    max_value = df.select(spark_max(col("Value"))).collect()[0][0]
 
-data = [(i,) for i in range(1001)]
-df = spark.createDataFrame(data, ["Value"])
+    # Normalize the "Value" column to the range [0, 1]
+    df_normalized = df.withColumn(
+        "NormalizedValue", (col("Value") - min_value) / (max_value - min_value)
+    )
 
-# Cache dataframe so subsequent operations are faster
-df.cache()
-
-# Show the original DataFrame
-print("Original DataFrame:")
-df.show()
-
-# Show the number of partitions
-print("Number of partitions:", df.rdd.getNumPartitions())
+    # Show the normalized DataFrame
+    print("Normalized DataFrame:")
+    df_normalized.show()
 
 
-# Compute the minimum and maximum values of the "Value" column
-min_value = df.select(spark_min(col("Value"))).collect()[0][0]
-max_value = df.select(spark_max(col("Value"))).collect()[0][0]
+if __name__ == "__main__":
+    # Initialize Spark session
+    spark = (
+        SparkSession.builder.appName("ExampleApp")  # type: ignore
+        .master("spark://localhost:7077")
+        .getOrCreate()
+    )
 
+    data = [(i,) for i in range(1001)]
+    df = spark.createDataFrame(data, ["Value"])
 
-# Normalize the "Value" column to the range [0, 1]
-df_normalized = df.withColumn(
-    "NormalizedValue", (col("Value") - min_value) / (max_value - min_value)
-)
+    # Cache dataframe so subsequent operations are faster
+    df.cache()
 
-# Show the normalized DataFrame
-print("Normalized DataFrame:")
-df_normalized.show()
+    # Show the original DataFrame
+    print("Original DataFrame:")
+    df.show()
 
-spark.stop()
+    # Show the number of partitions
+    print("Number of partitions:", df.rdd.getNumPartitions())
+
+    normalize_dataframe(df)
+
+    spark.stop()
