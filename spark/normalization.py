dec68008fc60cb0e2f3b30c96bf5de50d0077a23
Galen Gong <galengong@gmail.com>
2024-09-02 19:09:01 -0400

    Move FastAPI code to different repo
    Add spark and numpy test code
    
    Add docker-compose for running spark locally in container

---
+++ spark/normalization.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, min as spark_min, max as spark_max
+
+
+# Initialize Spark session
+spark = (
+    SparkSession.builder.appName("ExampleApp")  # type: ignore
+    .master("spark://localhost:7077")
+    .getOrCreate()
+)
+
+data = [(i,) for i in range(1001)]
+df = spark.createDataFrame(data, ["Value"])
+
+# Cache dataframe so subsequent operations are faster
+df.cache()
+
+# Show the original DataFrame
+print("Original DataFrame:")
+df.show()
+
+# Show the number of partitions
+print("Number of partitions:", df.rdd.getNumPartitions())
+
+
+# Compute the minimum and maximum values of the "Value" column
+min_value = df.select(spark_min(col("Value"))).collect()[0][0]
+max_value = df.select(spark_max(col("Value"))).collect()[0][0]
+
+
+# Normalize the "Value" column to the range [0, 1]
+df_normalized = df.withColumn(
+    "NormalizedValue", (col("Value") - min_value) / (max_value - min_value)
+)
+
+# Show the normalized DataFrame
+print("Normalized DataFrame:")
+df_normalized.show()
+
+spark.stop()
